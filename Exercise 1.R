8549311a5b0f1e7a688ec3753f0dc84620fd19ed
radiotelescopes <spam.aflow@simplelogin.fr>
2021-08-23 12:04:58 +0200

    Exercise 1

---
+++ Exercise 1.R
@@ -0,0 +1,39 @@
+# Run the following script. What do you think they do?
+library(lubridate)
+
+library(tidyverse)
+
+
+lubridate::date()
+
+lubridate::today()
+
+
+
+lubridate::month(lubridate::today())
+
+
+date()
+
+today()
+
+month(today())
+
+
+
+d <- '2011-12-12'
+
+d2 <- ymd(d)
+
+class(d)
+
+class(d2)
+
+
+
+Sys.Date()
+
+
+
+Sys.sleep(20)
+
